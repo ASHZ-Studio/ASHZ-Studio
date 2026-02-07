a309624f4f3f89b4fd442154e791a7f342daf169
hunain02 <hunainghayas.786@gmail.com>
2026-02-07 21:27:44 +0500

    Initial static website

---
+++ config.js
@@ -0,0 +1,6 @@
+// Global WhatsApp configuration
+// Update this number to change it everywhere in the site.
+// Expected format: country code + number, no + or leading zeros, e.g. 923001234567
+const WHATSAPP_NUMBER = "923082855273";
+
+export default WHATSAPP_NUMBER;
